3e34960bd23458ee8c7b61735cfe2dc126bb2b3a
kennby <153956421+kennby@users.noreply.github.com>
2024-04-12 17:07:24 -0500

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -4,19 +4,19 @@ let handler = async (m, { command, conn, text }) => {
     if (!text) throw `[❗INFO❗] NOMBRE DE LA CANCION FALTANTE, POR FAVOR INGRESE EL COMANDO MAS EL NOMBRE/TITULO O ENLACE DE ALGUNA CANCION O VIDEO DE YOUTUBE\n\n*—◉ EJEMPLO:\n#play.1 Good Feeling - Flo Rida*`;
 
     try {
-        let apikey = 'GATADIOS'; // Reemplaza 'TU_API_KEY_AQUI' con tu API key
+        let apikey = 'gatadios'; // Reemplaza 'TU_API_KEY_AQUI' con tu API key
         let res = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${apikey}&query=${encodeURIComponent(text)}`);
         let json = await res.json();
 
         if (json.status == 200 && json.message == "success") {
-            if (command == 'play.1') {
-                conn.reply(m.chat, `*_⏳ SE ESTÁ PROCESANDO SU AUDIO...⏳_*`, m);
-                conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' });
-            }
+            let url = command == 'play.1' ? json.result.audio : json.result.video;
+            let mimetype = command == 'play.1' ? 'audio/mp4' : 'video/mp4';
 
-            if (command == 'play.2') {
-                conn.reply(m.chat, `*_⏳ SE ESTÁ PROCESANDO SU VIDEO...⏳_*`, m);
-                conn.sendFile(m.chat, json.result.dlmp4, 'error.mp4', null, m);
+            if (url) {
+                conn.reply(m.chat, `*_⏳ SE ESTÁ PROCESANDO SU AUDIO/VIDEO...⏳_*`, m);
+                conn.sendFile(m.chat, url, 'media', '', m, false, { mimetype });
+            } else {
+                throw '[❗INFO❗] ERROR: URL del archivo no encontrada en la respuesta de la API';
             }
         } else {
             throw `[❗INFO❗] ERROR: ${json.message}`;
@@ -32,3 +32,4 @@ handler.tags = ['downloader'];
 handler.command = ['play.1', 'play.2'];
 
 export default handler;
+
