344499e8b5f81d049e86d16f1e68812fb4f1dcc1
kennby <153956421+kennby@users.noreply.github.com>
2024-04-12 16:56:45 -0500

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,56 +1,20 @@
-
 import fetch from 'node-fetch'
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-if (!text) throw `*[❗INFO❗] NOMBRE DE LA CANCION FALTANTE, POR FAVOR INGRESE EL COMANDO MAS EL NOMBRE /TITULO DE UNA CANCION*\n\n*—◉ EJEMPLO:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`
-let vid = (await youtubeSearch(text)).video[0]
-if (!vid) throw '*[❗INFO❗] LO SIENTO, NO SE PUDO ENCONTRAR EL AUDIO/ VIDEO, INTENTE CON OTRO NOMBRE/TITULO*'
+let handler = async (m, {command, conn, text}) => {
+if (!text) throw `[❗INFO❗] NOMBRE DE LA CANCION FALTANTE, POR FAVOR INGRESE EL COMANDO MAS EL NOMBRE/TITULO O ENLACE DE ALGUNA CANCION O VIDEO DE YOUTUBE\n\n*—◉ EJEMPLO:\n#play.1 Good Feeling - Flo Rida*`
 try {
-let { title, description, thumbnail, videoId, durationH, viewH, publishedTime } = vid
-const urll = 'https://www.youtube.com/watch?v=' + videoId
-var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
-var document = doc[Math.floor(Math.random() * doc.length)]
-const buttons = `
-*🎵 𝐀𝐔𝐃𝐈𝐎𝐃𝐎𝐂 🎵* => ${usedPrefix}ytmp3doc ${urll}
-*🎥 𝐕𝐈𝐃𝐄𝐎𝐃𝐎𝐂 🎥* => ${usedPrefix}ytmp4doc ${urll}
-*📋 𝐌𝐀𝐒 𝐑𝐄𝐒𝐔𝐋𝐓𝐀𝐃𝐎𝐒 📋* => ${usedPrefix}playlist ${text}`
-let texto1 = `*◉—⌈🔊 PLAY DOCUMENT 🔊⌋—◉*\n
-❏ 📌 *TITULO:* ${title}
-❏ 📆 *PUBLICADO:* ${publishedTime}
-❏ ⌚ *DURACION:* ${durationH}
-❏ 👀 *VISTAS:* ${viewH}
-❏ 📇 *DESCRIPCION:* ${description}
-❏ 🔗 *LINK:* ${urll}`.trim()
-let buttonMessage = `{ "document": { url: ${md}}, "fileName": '❏ 🌿 REPRODUCTOR DE YOUTUBE', "mimetype": 'application/vnd.ms-excel', "caption": texto1, "fileLength": '99999999999999', "mentions": [m.sender], "footer": wm, "buttons": buttons, "headerType": 4, contextInfo: { "mentionedJid": [m.sender], "externalAdReply": { "showAdAttribution": true, "title": ${title}, "mediaType": 2, "previewType": "VIDEO", "thumbnail": await (await fetch(thumbnail)).buffer(), "mediaUrl": ${urll}, "sourceUrl": ${urlgofc} }}} `
-let txt = '';
-let count = 0;
-for (const c of texto1+'\n'+buttons+'\n'+buttonMessage) {
-    await new Promise(resolve => setTimeout(resolve, 5));
-    txt += c;
-    count++;
-
-    if (count % 10 === 0) {
-        conn.sendPresenceUpdate('composing' , m.chat);
-    }
-}
-    await conn.sendMessage(m.chat, { text: txt.trim(), mentions: conn.parseMention(txt) }, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100} )
-} catch {
-    throw '*[❗INFO❗] ERROR, POR FAVOR VUELAVA A INTENTARLO*'}
-try {  
-let vid2 = await (await fetch(API('rrul', '/api/yt/yts', { q: text }))).json()
-let { url, title, description, image, seconds, timestamp, ago, views } = await vid2.result[0]
-let ytLink = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=Gatadios&query=${text}`)
-let jsonn = await ytLink.json()
-let aud = await jsonn.result.audio
-let capt = `❏ 📌 *TITULO:* ${title}\n❏ 📆 *PUBLICADO:* ${ago}\n❏ ⌚ *DURACION:* ${timestamp}\n❏ 👀 *VISTAS:* ${views.toLocaleString()}\n❏ 🔗 *LINK:* ${url}`
-const buttons = [{buttonId: `#playlist ${title}`, buttonText: {displayText: '📋 MAS RESULTADOS 📋'}, type: 1}]
-const buttonMessage = { image: {url: image}, caption: capt, footer: '*ENVIANDO AUDIO, AGUARDE UN MOMENTO...*', buttons: buttons, headerType: 4 }
-let msg = await conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-conn.sendMessage(m.chat, { document: { url: aud }, mimetype: 'audio/mpeg', fileName: `${title}.mp3`}, {quoted: msg})
-} catch {  
-throw '*[❗INFO❗] ERROR, POR FAVOR VUELAVA A INTENTARLO*'}
-}
-
-handler.help = ['playdoc', 'play3'].map(v => v + ' <pencarian>')
+let res = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=Gatadios&query=${text}`)
+if (command == 'play.1') {
+conn.reply(m.chat, `*_⏳ SE ESTÁ PROCESANDO SU AUDIO...⏳_*`, m)  
+let json = await res.json()
+conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
+if (command == 'play.2') {
+conn.reply(m.chat, `*_⏳ SE ESTÁ PROCESANDO SU VIDEO...⏳_*`, m)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.dlmp4, 'error.mp4', wm, m)}
+}catch(e){
+m.reply('*[❗INFO❗] ERROR, POR FAVOR VUELVA A INTENTARLO*')
+}}
+handler.help = ['play.1' , 'play.2'].map(v => v + ' <texto>')
 handler.tags = ['downloader']
-handler.command = /^play3|playdoc?$/i
+handler.command = ['play.1', 'play.2']
 export default handler
