3c396fc2c1d1667f458c2fae01462bb13ea292c2
kennby <153956421+kennby@users.noreply.github.com>
2024-04-13 17:34:26 -0500

    Update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(` *${mssg.uptime}*  \n\n${muptime}`) 
+   m.reply(`> *${mssg.uptime}*  \n\n${muptime}`) 
 }
 handler.help = ['runtime'] 
 handler.tags = ['main']
