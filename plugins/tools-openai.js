3bd3a7f139784e6f4814745733ab73e1d301f60c
kennby <153956421+kennby@users.noreply.github.com>
2024-04-14 07:20:04 -0500

    Update tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -1,50 +1,27 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
-import translate from '@vitalets/google-translate-api';
-import {Configuration, OpenAIApi} from 'openai';
-const configuration = new Configuration({organization: global.openai_org_id, apiKey: global.openai_key});
-const openaiii = new OpenAIApi(configuration);
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (usedPrefix == 'a' || usedPrefix == 'A') return;
-  if (!text) throw `INGRESE UNA PETICIÓN PARA ENVIAR A LA API`;
-  try {
-        conn.sendPresenceUpdate('composing', m.chat);
-        //let sistema1 = await fetch(`https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt`).then(v => v.text());
-        let sistema1 = `Actuaras como un Bot de WhatsApp el cual fue creado por BrunoSobrino, tu seras The Mystic - Bot.`;
-        async function getOpenAIChatCompletion(texto) {
-        const openaiAPIKey = global.openai_key;
-        let chgptdb = global.chatgpt.data.users[m.sender];
-        chgptdb.push({ role: 'user', content: texto });
-        const url = "https://api.openai.com/v1/chat/completions";
-        const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-        const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
-        const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
-        const result = await response.json();
-        const finalResponse = result.choices[0].message.content;
-        return finalResponse;
-        };
-        let respuesta = await getOpenAIChatCompletion(text);
-        if (respuesta == 'error' || respuesta == '' || !respuesta) return XD; // causar error undefined para usar otra api
-        m.reply(`${respuesta}`.trim());
-    } catch {
-      try {
-        conn.sendPresenceUpdate('composing', m.chat);
-        const botIA222 = await openaiii.createCompletion({model: 'text-davinci-003', prompt: text, temperature: 0.3, max_tokens: 4097, stop: ['Ai:', 'Human:'], top_p: 1, frequency_penalty: 0.2, presence_penalty: 0});
-        if (botIA222.data.choices[0].text == 'error' || botIA222.data.choices[0].text == '' || !botIA222.data.choices[0].text) return XD; // causar error undefined para usar otra api
-        m.reply(botIA222.data.choices[0].text.trim());
-    } catch {
-      try {
-        conn.sendPresenceUpdate('composing', m.chat);
-        const syms1 = `Actuaras como un Bot de WhatsApp el cual fue creado por BrunoSobrino, tu seras The Mystic - Bot.`;
-        const Empireapi1 = await fetch(`https://api.cafirexos.com/api/chatgpt?text=${text}&name=${m.name}&prompt=${syms1}`);
-        const empireApijson1 = await Empireapi1.json();
-        if (empireApijson1.resultado == 'error' || empireApijson1.resultado == '' || !empireApijson1.resultado) return XD; // causar error undefined para lanzar msg de error
-        m.reply(`${empireApijson1.resultado}`.trim());
-    } catch {
-      throw `ERROR DE API`;
-    }
-   }
- }
-};
-handler.command = /^(openai|chatgpt|ia|robot|openai2|chatgpt2|ia2|robot2|Mystic|MysticBot)$/i;
-export default handler;
+import fetch from 'node-fetch'
+
+var handler = async (m, { text,  usedPrefix, command }) => {
+
+if (!text) return conn.reply(m.chat, `🎌 *Ingresé una petición*\n\nEjemplo, !bard Conoces CuriosityBot-MD?`, m, fake, )
+
+try {
+
+conn.sendPresenceUpdate('composing', m.chat)
+var apii = await fetch(`https://api.alyachan.dev/api/bard-google-ai?q=${encodeURIComponent(text)}&apikey=GataDios`)
+var res = await apii.json()
+await m.reply(res.result)
+
+} catch (error) {
+console.error(error)
+return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
+}
+
+}
+handler.command = ['ia']
+handler.help = ['bard']
+handler.tags = ['ai']
+
+handler.premium = false
+
+export default handler
+
