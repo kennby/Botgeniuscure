3b3b323086da8b8c3925438f008eb465b28b9831
kennby <153956421+kennby@users.noreply.github.com>
2024-04-17 00:53:58 -0500

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -1,62 +1,47 @@
-import fetch from 'node-fetch';
-import yts from 'yt-search';
-import ytdl from 'ytdl-core';
-import axios from 'axios';
-
-const lolkeysapi = 'GataDios';
-
-const handler = async (m, {command, usedPrefix, conn, text}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`;
+const axios = require('axios')
+const yts = require('yt-search')
+let handler = async (m, {
+  usedPrefix,
+  command,
+  text
+}) => {
+  if (!text) return m.reply(Func.example(usedPrefix, command, 'Utopia'))
   try {
-    if (command == 'play.1') {
-      conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴀᴜᴅɪᴏ...⏳_*`, m);
-      try {
-        const mediaa = await ytPlay(text);
-        const audiocore = mediaa.result2?.[0]?.audio || mediaa.result2?.[1]?.audio || mediaa.result2?.[2]?.audio || null;
-        const aa = await conn.sendMessage(m.chat, {audio: {url: audiocore}, fileName: `error.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
-        if (!aa) {
-          throw new Error('*[❗] El primer método falló, intentando otro...*');
-        }        
-      } catch {
-        const res = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`);
-        const json = await res.json();
-        const aa_1 = await conn.sendMessage(m.chat, {audio: {url: json.result.audio}, fileName: `error.mp3`, mimetype: 'audio/mpeg'}, {quoted: m});
-        if (!aa_1) aa_1 = await conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, {mimetype: 'audio/mpeg'});
-      }
-    }
-    if (command == 'play.2') {
-      conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m);
-      try {
-        const mediaa = await ytPlayVid(text);
-        const aa_2 = await conn.sendMessage(m.chat, {video: {url: mediaa.result}, fileName: `error.mp4`, caption: `_𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭_`, thumbnail: mediaa.thumb, mimetype: 'video/mp4'}, {quoted: m});
-        if (!aa_2) {
-          throw new Error('*[❗] El primer método falló, intentando otro...*');
-        }
-      } catch {
-        const res = await fetch(`https://api.lolhuman.xyz/api/ytplay2?apikey=${lolkeysapi}&query=${text}`);
-        const json = await res.json();
-        await conn.sendFile(m.chat, json.result.video, 'error.mp4', `_𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭_`, m);
-      }
-    }
-  } catch {
-    throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*';
+    let yt = await (await yts(text)).all
+    let json = await Func.fetchJson(API('alya', '/api/yta', { url: yt[0].url }, 'apikey'))
+    m.react('🕐')
+    if (!json.status) return m.reply(Func.jsonFormat(json))
+    let ca = `乂  *Y T - P L A Y*\n\n`
+    ca += `  ∘  *Title* : ` + json.title + `\n`
+    ca += `  ∘  *Duration* : ` + json.duration + `\n`
+    ca += `  ∘  *Viewer* : ` + json.views + `\n`
+    ca += `  ∘  *Size* : ` + json.data.size + `\n\n`
+    ca += global.set.footer
+    let xSize = Func.sizeLimit(json.data.size, global.set.max_upload)
+    if (xSize.oversize) return m.reply(`The file size (${json.data.size}) is too large, please download it yourself via this link : ${await (await Func.shortlink(json.data.url))}`)
+    conn.sendMessageModify(m.chat, ca, m, {
+      largeThumb: true,
+      thumbnail: json.thumbnail
+    }).then(async () => {
+      // document
+      conn.sendMessage(m.chat, {
+        document: { url: json.data.url },
+        mimetype: 'audio/mp3',
+        fileName: json.title + '.mp3'
+      }, { quoted: m })
+      // audio
+      /*conn.sendMessage(m.chat, { 
+        audio: { url: json.data.url }, 
+        fileName: json.title + '.mp3', 
+        mimetype: 'audio/mpeg'
+      }, { quoted: m })*/
+    })
+  } catch (e) {
+    console.log(e)
+    return m.reply(Func.jsonFormat(e))
   }
-};
-
-// Funciones ytMp3, ytMp4, ytPlay y ytPlayVid sin cambios
-
-handler.help = ['play.1', 'play.2'].map((v) => v + ' <texto>');
-handler.tags = ['downloader'];
-handler.command = ['play.1', 'play.2'];
-export default handler;
-
-function bytesToSize(bytes) {
-  return new Promise((resolve, reject) => {
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes === 0) return 'n/a';
-    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-    if (i === 0) resolve(`${bytes} ${sizes[i]}`);
-    resolve(`${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`);
-  });
-      }
-
+}
+handler.help = handler.command = ['play']
+handler.tags = ['downloader']
+handler.limit = 1
+module.exports = handler
