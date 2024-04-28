822a5a7fb9b4581dff0713307adf22875bad90fd
kennby <153956421+kennby@users.noreply.github.com>
2024-04-28 18:40:23 -0500

    Update dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -1,44 +1,24 @@
+
 import { youtubeSearch } from '@bochilteam/scraper'
 import yts from 'yt-search'
-
-// Manejador para buscar videos de YouTube y obtener enlaces de descarga
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-    // Verifica si el usuario proporcionó un término de búsqueda
-    if (!text) throw `usa /play Lil Peep hate my life`
-
-    // Reacciona al mensaje con un emoji de disco
+    if (!text) throw `✳️ ${mssg.example} *${usedPrefix + command}* Lil Peep hate my life`
     m.react('📀')
-
-    // Realiza la búsqueda en YouTube
     let result = await yts(text)
     let ytres = result.videos
     let listSections = []
-
-    // Mapea los resultados de la búsqueda y los agrega a la lista de secciones
-    Object.values(ytres).map((v, index) => {
-        listSections.push([`${index}┃ ${v.title}`, [
-            ['🎶 MP3', `${usedPrefix}mp3 ${v.url}`, `${mssg.title}\n`],
-            ['🎥 MP4', `${usedPrefix}mp4 ${v.url}`, `${mssg.title}\n`]
+	Object.values(ytres).map((v, index) => {
+	listSections.push([`${index}┃ ${v.title}`, [
+          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`],
+          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ ⌚ *${mssg.duration}:* ${v.timestamp}\n▢ 👀 *${mssg.views}:* ${v.views}\n▢ 📌 *${mssg.title}* : ${v.title}\n▢ 📆 *${mssg.aploud}:* ${v.ago}\n`]
         ]])
-    })
-
-    // Envía la lista de resultados al chat
-    return conn.sendList(m.chat, 'Resultados de:\n *${text}*`, mssg.ig, `k gay`, listSections, m)
+	})
+	return conn.sendList(m.chat, '  ≡ *FG MUSIC*🔎', `\n 📀 Resultados de:\n *${text}*`, mssg.ig, `Click Aqui`, listSections, m)
 }
-
-// Información sobre cómo usar el comando
 handler.help = ['play2']
-
-// Etiquetas relacionadas con el comando
 handler.tags = ['dl']
+handler.command = ['play2', 'playvid2', 'playlist', 'playlista'] 
+handler.disabled = true
 
-// Comandos relacionados con este manejador
-handler.command = ['play2', 'playvid2', 'playlist', 'playlista']
-
-// Indica si este comando está desactivado
-handler.disabled = false
-
-// Exporta el manejador para ser utilizado por otros archivos
 export default handler
-  
