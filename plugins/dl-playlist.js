5d6e768da9e5e0388549420c6d3139967d1606f3
kennby <153956421+kennby@users.noreply.github.com>
2024-04-28 18:45:01 -0500

    Update dl-playlist.js

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -38,7 +38,7 @@ handler.tags = ['dl']
 handler.command = ['play2', 'playvid2', 'playlist', 'playlista']
 
 // Indica si este comando está desactivado
-handler.disabled = true
+//handler.disabled = true
 
 // Exporta el manejador para ser utilizado por otros archivos
 export default handler
